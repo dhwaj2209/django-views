a2fe5bb71bd0b9c018339f52130d6d01f5851b24
dhwaj2209 <dhwaj2209@gmail.com>
2025-09-27 15:17:12 +0530

    ulr patterns added

--- shopping/cart/urls.py
+++ shopping/cart/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('function', views.index, name='index'),
+    #path('function', views.index, name='index'),
     #path('', views.date_view),
-    path('class', views.cart_list.as_view())
+    path('class', views.cart_list.as_view(), name='list'),
+    path('', views.home_view, name='home'),
+    path('about/', views.about_view, name='about'),
+    path('contact/', views.contact_view, name='contact'),
+    path('item/<int:item_id>/', views.item_detail, name='item_detail'),
+    path('create/', views.item_create, name='create'),
 ] 
--- shopping/cart/views.py
+++ shopping/cart/views.py
@@ -2,8 +2,29 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import datetime
 from django.views.generic.list import ListView
+from django.shortcuts import redirect
+from django.urls import reverse
 
 from .models import CartModel
+
+def home_view(request):
+    return render(request, 'home.html')
+
+def about_view(request):
+    return render(request, 'about.html')
+
+def contact_view(request):
+    return render(request, 'contact.html')
+
+def item_detail(request, item_id):
+    item = CartModel.objects.get(id=item_id)
+    context = {'data': item }
+    return render(request, "item_details.html", context)
+
+def item_create(request):
+    CartModel.objects.create(title="Chess", description="Sports")
+    new_item_id = 5  
+    return redirect(reverse('item_detail', kwargs={'item_id': new_item_id}))
 
 def index(request):
     #return HttpResponse("Hi, Django")
